7ec1e5bee559b46f3412bbf1011a095e5a20f27a
Michał Murawski <m.a.murawski@wp.pl>
2022-11-02 22:04:44 +0100

    task color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyHtml = document.querySelector('body');
 
+let timerChangeColor = null;
+
+btnStart.addEventListener('click', () => {
+    timerChangeColor = setInterval(() => {
+        bodyHtml.style.backgroundColor=getRandomHexColor();
+    }, 1000);
+    btnStart.setAttribute('disabled','');
+})
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerChangeColor);
+    btnStart.removeAttribute('disabled','');
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
